d0eecec4ddf73fb3cf2f0d9e24213fdded8ec058
chandjamba <chandjamba2001@gmail.com>
2024-06-11 16:38:39 -0400

    Working on inventoryitemdetail styling

--- src/components/InventoryItemDetail.jsx
+++ src/components/InventoryItemDetail.jsx
@@ -16,38 +16,13 @@ export default function InventoryItemDetail() {
     fetchedInventoryItemData();
   }, [itemId]);
   return (
-    <div className="inventoryItemDetail">
-      <div className="inventory">
-        <h1 className="inventory__main-heading">
-          {inventoryItemDetail?.item_name}
-        </h1>
-        <button className="inventory-edit">Edit</button>
-
-        <div className="inventoryItemDetail">
-          <div className="inventory__item" key={inventoryItemDetail?.id}>
-            <div className="inventory__info">
-              <div className="inventory__info-box1">
-                <p className="inventory__category-heading">CATEGORY</p>
-                <div className="inventory__category">
-                  {inventoryItemDetail?.category}
-                </div>
-                <p className="inventory__quantity-heading">QTY</p>
-                <div className="inventory__quantity">
-                  {inventoryItemDetail?.quantity}
-                </div>
-              </div>
-              <div className="inventory__info-box2">
-                <p className="inventory__status-heading">STATUS</p>
-                <div className="inventory__status">
-                  {inventoryItemDetail?.status}
-                </div>
-                <p className="inventory__heading">WAREHOUSE</p>
-                <div className="inventory__location">
-                  {inventoryItemDetail?.warehouse_name}
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="inventory">
+      <div className="inventory__item-details">
+        <div className="heading__editBtn-container">
+          <h1 className="inventory__item-heading">
+            {inventoryItemDetail?.item_name}
+          </h1>
+          <button className="inventory__item-edit">Edit</button>
         </div>
       </div>
     </div>
