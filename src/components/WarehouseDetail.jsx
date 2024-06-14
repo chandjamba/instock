cdedbebdc8a597a16d23789ed61dfcfbba5f74ed
chandjamba <chandjamba2001@gmail.com>
2024-06-14 05:21:43 -0400

    successfully completed warehouse detail part.

--- src/components/WarehouseDetail.jsx
+++ src/components/WarehouseDetail.jsx
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./warehouseDetail.scss";
 
-const url2 = "https://instock-api-cj.onrender.com/api/inventories";
 
 export default function WarehouseDetail() {
   const [warehouseDetail, setWarehouseDetail] = useState();
-  const [sameWarehouses, setSameWarehouses] = useState();
+  const [sameWarehouse, setSameWarehouse] = useState();
   const { warehouseId } = useParams();
-
+  
   useEffect(() => {
     async function fetchedWarehousesDetail() {
       const url = `https://instock-api-cj.onrender.com/api/warehouses/${warehouseId}`;
@@ -19,16 +18,17 @@ export default function WarehouseDetail() {
     }
     fetchedWarehousesDetail();
   }, [warehouseId]);
-
+  
   useEffect(() => {
-    async function fetchedSameWarehouses() {
+    async function fetchedSameWarehouse() {
+      const url2 = `https://instock-api-cj.onrender.com/api/warehouses/${warehouseId}/inventories`;
       const resp = await fetch(url2);
       const data = await resp.json();
       console.log(data);
-      setSameWarehouses(data);
+      setSameWarehouse(data);
     }
-    fetchedSameWarehouses();
-  }, []);
+    fetchedSameWarehouse();
+  }, [warehouseId]);
 
   return (
     <div className="warehouses">
@@ -39,6 +39,7 @@ export default function WarehouseDetail() {
           </h1>
           <button className="warehouse__editBtn">Edit</button>
         </div>
+        <div className="warehouse__info-card">
         <div className="warehouse__detail-card">
           <div className="warehouse__detail-info-box1">
             <p className="warehouse__detail-address-heading">
@@ -80,9 +81,39 @@ export default function WarehouseDetail() {
             </div>
           </div>
         </div>
+        </div>
       </div>
-      <div className="warehouse__items">
+      <div className="warehouse__inventories">
+        {sameWarehouse.map((warehouse)=> {
+          return(
+      <div className="warehouse__inventories-card">
+        <div className="warehouse__info-containers">
+          <div className="warehouse__info-container4">
+          <p className="warehouse__inventory-item-heading">INVENTORY ITEM</p>
+                    <div
+                      className="warehouse__inventory-item-name"
+                      // to={`/inventory/${inventory?.id}`}
+                    >
+                      {warehouse?.item_name}
+                    </div>
+                    <p className="warehouse__inventory-category-heading">CATEGORY</p>
+                    <div className="warehouse__inventory-category">
+                      {warehouse?.category}
+                    </div>
+          </div>
+          <div className="warehouse__info-container5">
+          <p className="warehouse__inventory-status-heading">STATUS</p>
+                    <div className="warehouse__inventory-status">{warehouse?.status}</div>
+                    <p className="warehouse__inventory-quantity-heading">QUANTITY</p>
+                    <div className="warehouse__inventory-quantity">
+                      {warehouse?.quantity}
+                    </div>
+          </div>
+        </div>
       </div>
+          );
+  })}
+    </div>
     </div>
   );
 }
